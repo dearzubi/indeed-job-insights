5cfc5a2f55fb350d03b6e1c9ffbafca4f4b9dcdc
coolpengwing <coolpengwing@gmail.com>
2026-04-22 13:06:56 +0100

    build: add Lefthook hooks and Conventional Commits lint

---
+++ commitlint.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "chore", "refactor", "test", "build", "ci", "perf", "style", "revert"],
+    ],
+    "subject-case": [0],
+    "header-max-length": [2, "always", 100],
+  },
+};
